2f739edd9d3a2df8e86bff5873a80308a904be22
plaetzchen79 <andre.wollny@aareon.com>
2019-11-18 18:13:31 +0100

    Initial import

---
+++ SoundExActivity/SoundExCustomActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace SoundExActivity
+{
+    public class SoundExCustomActivity: CodeActivity
+    {
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> Phrase { get; set; }
+
+        [Category("Input")]
+        public InArgument<bool> UseGernan { get; set; } = false;
+
+        [Category("Output")]
+        public OutArgument<string> Code { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            // ToDo..
+        }
+    }
+}
