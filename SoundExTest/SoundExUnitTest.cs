bb87d137d8c0557242579b957c2b95300fb1f9b2
plaetzchen79 <andre.wollny@aareon.com>
2019-11-18 19:10:06 +0100

    Add test project

---
+++ SoundExTest/SoundExUnitTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoundExActivity;
+
+namespace SoundExTest
+{
+    [TestClass]
+    public class SoundExUnitTest
+    {
+        [TestMethod]
+        public void TestCodes()
+        {
+            SoundExCustomActivity soundExAct = new SoundExCustomActivity();
+        }
+    }
+}
